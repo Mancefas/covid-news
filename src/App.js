a91e569bdae758de70a2abc687795f4442538d58
Mancefas <mantvydasliobe@gmail.com>
2021-10-11 19:49:46 +0300

    header added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import APIData from "./API/APIData";
 import DataContainer from "./Components/DataContainer";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <APIData />
       <DataContainer />
     </>
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={classes.header}>
+      <h2>News about covid in Baltic states</h2>
+    </header>
+  );
+};
+
+export default Header;
