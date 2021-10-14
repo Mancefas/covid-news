11ed2bd3a0dbaecdc58f480ced27e4d35eabc480
Mancefas <mantvydasliobe@gmail.com>
2021-10-14 09:15:16 +0300

    added some CSS to world data container

--- src/Components/WorldDataContainer.js
+++ src/Components/WorldDataContainer.js
@@ -2,6 +2,9 @@ import React, { useContext } from "react";
 import { Container, Grid, Paper, CircularProgress } from "@mui/material";
 import classes from "./WorldDataContainer.module.css";
 import Context from "../store/Context";
+import AddReactionSharpIcon from "@mui/icons-material/AddReactionSharp";
+import CoronavirusIcon from "@mui/icons-material/Coronavirus";
+import AirlineSeatIndividualSuiteIcon from "@mui/icons-material/AirlineSeatIndividualSuite";
 
 const WorldDataContainer = () => {
   const context = useContext(Context);
@@ -9,7 +12,7 @@ const WorldDataContainer = () => {
     <Container className={classes.container}>
       {context.isLoadingInit && <CircularProgress color="inherit" />}
       {!context.isLoadingInit && (
-        <Grid container spacing={4}>
+        <Grid container spacing={1}>
           <Grid item xs={12} md={12}>
             <Paper elevation={6} className={classes.paper1}>
               <h3>World news ({context.initData["Last Update"]})</h3>
@@ -17,17 +20,23 @@ const WorldDataContainer = () => {
           </Grid>
           <Grid item xs={12} sm={4}>
             <Paper elevation={4} className={classes.paper_other}>
+              <CoronavirusIcon fontSize={"small"} color={"primary"} />
               Cases: {context.initData["Total Cases_text"]}{" "}
               {context.initData["New Cases_text"]}
             </Paper>
           </Grid>
           <Grid item xs={12} sm={4}>
             <Paper elevation={4} className={classes.paper_other}>
+              <AddReactionSharpIcon fontSize={"small"} color={"success"} />{" "}
               Recovered: {context.initData["Total Recovered_text"]}
             </Paper>
           </Grid>
           <Grid item xs={12} sm={4}>
             <Paper elevation={4} className={classes.paper_other}>
+              <AirlineSeatIndividualSuiteIcon
+                fontSize={"small"}
+                color={"error"}
+              />
               Deaths {context.initData["Total Deaths_text"]}{" "}
               {context.initData["New Deaths_text"]}{" "}
             </Paper>
